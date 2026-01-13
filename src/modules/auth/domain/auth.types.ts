a1b981d3cd90393dc5413e74b36e7534ebc55ca7
Joseph Infante <josephinfante.y@gmail.com>
2026-01-12 23:52:42 -0500

    feat(auth): add types and mapper for authentication module

---
+++ src/modules/auth/domain/auth.types.ts
@@ -0,0 +1,26 @@
+import { JWTPayload } from "jose";
+
+export interface SignUpInput {
+	firstName: string;
+	lastName: string;
+	email: string;
+	password: string;
+}
+
+export interface SignInInput {
+	email: string;
+	password: string;
+}
+
+export interface AuthResponse {
+	user: {
+		firstName: string;
+		lastName: string;
+		email: string;
+	};
+	token: string;
+}
+
+export interface VerifiedToken {
+	payload: JWTPayload & { valid: boolean; reason?: string };
+}
