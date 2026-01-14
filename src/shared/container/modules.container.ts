a096bf7f1bda625ad1b115f1e6af7eb5ce46ceb4
Joseph Infante <josephinfante.y@gmail.com>
2026-01-14 00:01:10 -0500

    feat(platforms): register platform module and add platform API routes

--- src/shared/container/modules.container.ts
+++ src/shared/container/modules.container.ts
@@ -1,5 +1,7 @@
 import { registerAuthModule } from "@modules/auth";
 import { createAuthApiRoutes } from "@modules/auth/infrastructure/http/api.routes";
+import { registerPlatformModule } from "@modules/platforms";
+import { createPlatformApiRoutes } from "@modules/platforms/infrastructure/http/api.routes";
 import { registerUserModule } from "@modules/users";
 import { createApiUserRoutes } from "@modules/users/infrastructure/http/api.routes";
 import { Router } from "express";
@@ -7,11 +9,13 @@ import { Router } from "express";
 export function registerAllModules() {
 	registerUserModule();
 	registerAuthModule();
+	registerPlatformModule();
 }
 
 export function getModuleRouters(): { path: string; router: Router }[] {
 	return [
 		{ path: "/api/users", router: createApiUserRoutes() },
 		{ path: "/api/auth", router: createAuthApiRoutes() },
+		{ path: "/api/platforms", router: createPlatformApiRoutes() },
 	];
 }
